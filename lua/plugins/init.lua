857ef468cc8b779d9f76b5916f1bb2fc1190f322
Daniel Tjandra <daniel.tjandra888@gmail.com>
2026-03-09 23:27:41 -0700

    Added telescope

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,8 +3,10 @@ vim.pack.add({
     'https://github.com/nvim-telescope/telescope.nvim',
     'https://github.com/sainnhe/sonokai',
     'https://github.com/neovim/nvim-lspconfig',
+    'https://github.com/nvim-lua/plenary.nvim',
 })
 
 -- Configuration in separate files
 require("plugins.colorscheme")
 require("plugins.lsp")
+require("plugins.telescope")
